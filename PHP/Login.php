84b98a4fff8c4482a6bafb6352d3362008d8794a
Erradish <130740646+Erradish@users.noreply.github.com>
2023-05-15 09:20:07 +0200

    Add files via upload

--- PHP/Login.php
+++ PHP/Login.php
@@ -3,7 +3,15 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: X-Request-with, Content-Type, Origin, Cache-Control, Pragma, Authorization");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header('Content-type: application/json');
-    function errore($response_code, $message)
+
+require_once 'jwt/src/BeforeValidException.php';
+require_once 'jwt/src/ExpiredException.php';
+require_once 'jwt/src/SignatureInvalidException.php';
+require_once 'jwt/src/JWT.php';
+
+use \Firebase\JWT\JWT; 
+
+    function risposta($response_code, $message)
     {
         http_response_code($response_code);
         header("Content-type: application/json; charset: UTF-8");
@@ -13,16 +21,18 @@ header('Content-type: application/json');
     $dati = json_decode(file_get_contents("php://input"));
     $email = $dati->email;
     $password = $dati->password;
-
-    if (isset($email))
-    {
- 
-        // connect with database
+    
         $hostname = "localhost";
         $dbname = "app";
         $user = "root";
         $pass = "";
         $db = new PDO ("mysql:host=$hostname;dbname=$dbname", $user, $pass);
+
+    if (isset($email))
+    {
+ 
+        // connect with database
+        
     
         // check if credentials are okay, and email is verified
         $sql = $db->prepare("SELECT * FROM user WHERE Mail = ?");
@@ -33,21 +43,55 @@ header('Content-type: application/json');
             array_push($result, $data);
         }
         if (count($result) == 0) {
-            errore(400, "Not registered");//Non registrato
+            risposta(400, "Not registered");//Non registrato
         }
         else{
             
             if (!password_verify($password, $result[0]["Password"]))
             {
-                errore(401,"Unauthorized"); //Password errata
+                risposta(401,"Unauthorized"); //Password errata
             }
     
             if ($result[0]["Verificato"] == false)
             {
-                errore(403,"Unauthorized");//Non è verificato, reindirizzata alla form di conferma
+                risposta(403,"Unauthorized");//Non è verificato, reindirizzata alla form di conferma
             }
     
-            errore(200,"Success");
+            
+            $issuedat_claim = time();  
+            $tempo = $issuedat_claim + 10;
+
+            $attuale = date("Y-m-d H:i:s", time());
+            $attuale = strtotime($attuale);
+
+            $durata = date("Y-m-d H:i:s", $tempo +10);
+            $durata = strtotime($durata);
+            $key = "asdaefasd";
+
+            $token = array(
+                "iat" => $attuale,
+                "jti"  => base64_encode(rand(0, 10000000)),
+                "nbf" => $tempo,
+                "exp"  => $durata,
+                "data" => array(
+                    "mail" => $email,
+                )
+            ); 
+        
+            $jwt = JWT::encode($token, $key);
+            
+            $sql = $db->prepare("UPDATE user SET Token = ? WHERE Mail = ?");
+            $sql->execute([$jwt,$email]);
+
+            $arr = array('token' => $jwt);
+            $sql = $db->prepare("SELECT Mail, UserName, Number, Img, Token FROM user WHERE Mail LIKE ?");
+            $sql->execute([$email]);
+
+            $result = array();
+            while ($data = $sql->fetch(PDO::FETCH_ASSOC)) {
+            array_push($result, $data);
+            }
+            echo json_encode($result[0]);
             exit();
         }
     }
